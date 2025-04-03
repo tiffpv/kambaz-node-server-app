c9196d557c0f288b126b977873cbc2d083f3539a
Tiffany Pacas Villanueva <tiffanypacas97@gmail.com>
2025-04-03 02:55:57 -0400

    recovered files

--- Kambaz/Enrollments/dao.js
+++ Kambaz/Enrollments/dao.js
@@ -5,3 +5,17 @@ export function enrollUserInCourse(userId, courseId) {
   const { enrollments } = Database;
   enrollments.push({ _id: uuidv4(), user: userId, course: courseId });
 }
+export function unenrollUserInCourse(userId, courseId) {
+  //const { enrollments } = Database;
+  Database.enrollments = Database.enrollments.filter(
+    (e) => !(e.user === userId && e.course === courseId)
+  );
+}
+
+export function findEnrollmentsForUser(userId) {
+  const { enrollments } = Database;
+  return enrollments.filter((enrollment) => enrollment.user === userId);
+}
+export function findAllEnrollments() {
+  return Database.enrollments;
+}
